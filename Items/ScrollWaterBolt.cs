7ea2324514988b4e879b01a5a2e418f18670742e
Zack <zackhemmingwayfox@gmail.com>
2020-12-02 21:56:24 -0500

    Scrolls Made and Work

---
+++ Items/ScrollWaterBolt.cs
@@ -0,0 +1,31 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace TynyransMod.Items
+{
+  public class ScrollWaterBolt : Scroll
+  {
+
+    public override void SetStaticDefaults()
+    {
+      DisplayName.SetDefault("Water Bolt Scroll");
+    }
+
+    public override void SetDefaults()
+    {
+      item.CloneDefaults(ItemID.WaterBolt);
+      base.SetDefaults();
+      item.damage = (int)(item.damage * 1.25);
+    }
+
+    public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+    {
+      Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(5));
+      Projectile.NewProjectile(position, perturbedSpeed, item.shoot, damage, knockBack, player.whoAmI);
+      return true;
+    }
+  }
+}
--- Utilities/TynUtils.cs
+++ Utilities/TynUtils.cs
@@ -7,59 +7,60 @@ using TynyransMod.Buffs;
 
 namespace TynyransMod
 {
-    public static class TynUtils
+  public static class TynUtils
+  {
+    public static Tynyran Tyn(this Player player) => (Tynyran)player.GetModPlayer<Tynyran>();
+    public static TynGlobalNPC Tyn(this NPC npc) => (TynGlobalNPC)npc.GetGlobalNPC<TynGlobalNPC>();
+    public static TynGlobalItem Tyn(this Item item) => (TynGlobalItem)item.GetGlobalItem<TynGlobalItem>();
+    public static TynGlobalProj Tyn(this Projectile proj) => (TynGlobalProj)proj.GetGlobalProjectile<TynGlobalProj>();
+
+    public static Vector2 RandomPointInHitbox(this Rectangle hitbox)
     {
-        public static Tynyran Tyn(this Player player) => (Tynyran) player.GetModPlayer<Tynyran>();
-        public static TynGlobalNPC Tyn(this NPC npc) => (TynGlobalNPC) npc.GetGlobalNPC<TynGlobalNPC>();
-        public static TynGlobalItem Tyn(this Item item) => (TynGlobalItem) item.GetGlobalItem<TynGlobalItem>();
-        public static TynGlobalProj Tyn(this Projectile proj) => (TynGlobalProj) proj.GetGlobalProjectile<TynGlobalProj>();
-
-        public static Vector2 RandomPointInHitbox(this Rectangle hitbox) {
-            Vector2 v = new Vector2();
-            int semiAxisX = Main.rand.Next(hitbox.Left, hitbox.Right), 
-                semiAxisY = Main.rand.Next(hitbox.Top, hitbox.Bottom);
-            v.X = semiAxisX;
-            v.Y = semiAxisY;
-            return v;
-        }
-
-        public static void Parry(Player player, Rectangle hitbox)
-        {
-            int NoOfProj = Main.projectile.Length;
-			int affectedProjs = 0;
-			for (int i = 0; i < NoOfProj; i++)
-			{
-				Projectile currProj = Main.projectile[i];
-				if (!player.HasBuff(ModContent.BuffType<CantDeflect>()) && currProj.active && currProj.hostile && hitbox.Intersects(currProj.Hitbox))
-				{
-					// Add your melee damage multiplier to the damage so it has a little more oomph
-					currProj.damage = (int)(currProj.damage * player.meleeDamageMult);
-					
-					// If Micit Bangle is equipped, add that multiplier.
-					currProj.damage = player.Tyn().micitBangle ? (int)(currProj.damage * 2.5) : currProj.damage;
-					// Convert the proj so you own it and reverse its trajectory
-					currProj.owner = player.whoAmI;
-					currProj.hostile = false;
-					currProj.friendly = true;
-					currProj.Tyn().deflected = true;
-					currProj.velocity.X = -currProj.velocity.X;
-					currProj.velocity.Y = -currProj.velocity.Y;
-					affectedProjs++;
-				}
-			}
-				if (affectedProjs > 0)
-				{
-					// Give a cooldown; 1 second per projectile reflected
-					// CantDeflect is a debuff, separate from this code block
-					player.AddBuff(ModContent.BuffType<CantDeflect>(), affectedProjs * 60, true);
-				}
-        }
-
-        public static void AddWithCondition<T>(this List<T> list, T type, bool condition)
-        {
-            if (!condition)
-            return;
-            list.Add(type);
-        }
+      Vector2 v = new Vector2();
+      int semiAxisX = Main.rand.Next(hitbox.Left, hitbox.Right),
+          semiAxisY = Main.rand.Next(hitbox.Top, hitbox.Bottom);
+      v.X = semiAxisX;
+      v.Y = semiAxisY;
+      return v;
     }
+
+    public static void Parry(Player player, Rectangle hitbox)
+    {
+      int NoOfProj = Main.projectile.Length;
+      int affectedProjs = 0;
+      for (int i = 0; i < NoOfProj; i++)
+      {
+        Projectile currProj = Main.projectile[i];
+        if (!player.HasBuff(ModContent.BuffType<CantDeflect>()) && currProj.active && currProj.hostile && hitbox.Intersects(currProj.Hitbox))
+        {
+          // Add your melee damage multiplier to the damage so it has a little more oomph
+          currProj.damage = (int)(currProj.damage * player.meleeDamageMult);
+
+          // If Micit Bangle is equipped, add that multiplier.
+          currProj.damage = player.Tyn().micitBangle ? (int)(currProj.damage * 2.5) : currProj.damage;
+          // Convert the proj so you own it and reverse its trajectory
+          currProj.owner = player.whoAmI;
+          currProj.hostile = false;
+          currProj.friendly = true;
+          currProj.Tyn().deflected = true;
+          currProj.velocity.X = -currProj.velocity.X;
+          currProj.velocity.Y = -currProj.velocity.Y;
+          affectedProjs++;
+        }
+      }
+      if (affectedProjs > 0)
+      {
+        // Give a cooldown; 1 second per projectile reflected
+        // CantDeflect is a debuff, separate from this code block
+        player.AddBuff(ModContent.BuffType<CantDeflect>(), affectedProjs * 60, true);
+      }
+    }
+
+    public static void AddWithCondition<T>(this List<T> list, T type, bool condition)
+    {
+      if (!condition)
+        return;
+      list.Add(type);
+    }
+  }
 }
